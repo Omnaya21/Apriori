4b4f6b50f8bce779c84a80322ec45fe06fcb15b3
Omar Navarro <onavarro@ingyso.com>
2020-11-14 18:04:32 -0800

    Changed bottom bar items for Apriori

--- app/src/main/java/com/ingyso/apriori/ui/tasks/TasksFragment.java
+++ app/src/main/java/com/ingyso/apriori/ui/tasks/TasksFragment.java
@@ -1,4 +1,4 @@
-package com.ingyso.apriori.ui.notifications;
+package com.ingyso.apriori.ui.tasks;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.ingyso.apriori.R;
 
-public class NotificationsFragment extends Fragment {
+public class TasksFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private TasksViewModel tasksViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        tasksViewModel =
+                new ViewModelProvider(this).get(TasksViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_tasks, container, false);
+        final TextView textView = root.findViewById(R.id.text_tasks);
+        tasksViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/ingyso/apriori/ui/tasks/TasksViewModel.java
+++ app/src/main/java/com/ingyso/apriori/ui/tasks/TasksViewModel.java
@@ -1,16 +1,16 @@
-package com.ingyso.apriori.ui.dashboard;
+package com.ingyso.apriori.ui.tasks;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class TasksViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public TasksViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is tasks fragment");
     }
 
     public LiveData<String> getText() {
